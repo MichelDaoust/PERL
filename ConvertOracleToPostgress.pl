40acaef2ed3f3be43cc1ae2192ab72acdff1136c
Michel Daoust <michel.daoust@cgi.com>
2018-03-26 14:07:03 -0400

    new stuff

--- ConvertOracleToPostgress.pl
+++ ConvertOracleToPostgress.pl
@@ -12,6 +12,7 @@ local $CurrentClass = "";
 local $IsInSQLQueryMode = 0;
 local $StartFunctionLine = 0;
 local $lineIndex = 0;
+local $CurrentQueryVariable = "";
 
 
 local %StatisticsOfCurrentFunction = ( 
@@ -21,11 +22,13 @@ local %StatisticsOfCurrentFunction = (
         "LOC" => 0,
         "SQLQueryOccurence" => 0,
         "StringBuilderOccurence" => 0,
+        "COUNT" => 0,
 
           
                                     );
 
 local %vQueryVariables = (); 
+local %vQuerySQL = (); 
 
 
 
@@ -78,14 +81,23 @@ sub matchDBStuff() {
 sub ScanSQLQueryObject()
 {
     $codeLine = $_[0];
-    if ( ($sqlQueryVariable) = $codeLine =~ m/^[ \t]*(?:SqlQuery){1}[ \t]+([A-Za-z_][A-Za-z\d_]*)/i)
+    if ( ($sqlQueryVariable) = $codeLine =~ m/^[ \t]*(?:final)?[ \t]*(?:SqlQuery){1}[ \t]+([A-Za-z_][A-Za-z\d_]*)/i)
     {
         $vQueryVariables{$sqlQueryVariable} = 0;
         $IsInSQLQueryMode = 1;
     }
 
+    #Scan for Constructor
+    if ( $codeLine =~ /(?:new){1}[ \t]+(?:SqlQuery){1}\(\"(.*)\"\)/i)
+    {
+        $vQuerySQL{$sqlQueryVariable} = $1;
+        print("%%%%%%%%%%%%%%%%%%%%%%%%%%%% SQL: $CurrentSQL");
+    }
+
 
 }
+
+
 
 sub ScanUsageOfQueryObject()
 {
@@ -93,10 +105,13 @@ sub ScanUsageOfQueryObject()
 
     if ($IsInSQLQueryMode == 1)
     {
+        ScanLineWhenQueryIsON($codeLine);
+
+
         foreach my $oneVariable (keys %vQueryVariables)
          {
             # look for a variable usage (ex :vQuery.{something})
-            if ( ($sqlQueryVariable) = $codeLine =~ m/^[ \t]*($oneVariable)\./i)
+            if ( ($sqlQueryVariable) = $codeLine =~ m/[ \t]*($oneVariable)\./i)
             {
                 if (exists ($vQueryVariables{$sqlQueryVariable}))
                 {
@@ -106,11 +121,60 @@ sub ScanUsageOfQueryObject()
                 {
                     $vQueryVariables{$sqlQueryVariable} = 0;
                 }
+                $CurrentQueryVariable = $sqlQueryVariable;
                 ProcessQueryVariable($codeLine, $sqlQueryVariable);
             }
         }
     }
     
+}
+
+
+sub ScanLineWhenQueryIsON()
+{
+    $codeLine = $_[0];
+    
+    if ($codeLine =~ /\.appendQuery/i)
+    {
+   
+        ProcessAppendQuery($codeLine);
+   
+        $StatisticsOfCurrentFunction{"SQLQueryOccurence"} = $StatisticsOfCurrentFunction{"SQLQueryOccurence"} + 1;  
+    }
+    if ($codeLine =~ /NVL/i)
+    {
+     #   print("SCANLINE : $codeLine\n");
+        $StatisticsOfCurrentFunction{"NVL"} = $StatisticsOfCurrentFunction{"NVL"} + 1;  
+    }
+    if ($codeLine =~ /count/i)
+    {
+        $StatisticsOfCurrentFunction{"COUNT"} = $StatisticsOfCurrentFunction{"COUNT"} + 1;  
+        
+    }
+    
+}
+
+
+sub ProcessAppendQuery()
+{
+    $codeLine = $_[0];
+
+    if ($codeLine =~ /\.appendQuery\("(.*)"\);/i)
+    {
+        my $SQL = $1; 
+        if ($codeLine =~ /([A-Za-z_][A-Za-z\d_]*)\.appendQuery/i)
+        {
+            $vQuerySQL{$1}  .= $SQL;
+        }
+        else
+        {
+            $vQuerySQL{$CurrentQueryVariable}  .= $SQL;
+
+        }
+    
+    }
+    
+
 }
 
 
@@ -136,34 +200,43 @@ sub LookCurrentFunctionAndClass() {
     {
         $CurrentClass = $matchClass;
         print("Match Class : $CurrentClass\n");
+        $NextIsFunctionName = 0;
     } 
     else
     {
         # match with or without static (?:[ \t]static[ \t])?
         if ($codeLine =~ m/^[ \t]*(?:private|protected|public)+(?:[ \t]static[ \t])?[ \t]+\b\w+\b/i) 
         {
-            $NextIsFunctionName = 1;
-            if (
-                (($matchFunctionName) = $codeLine =~ m/^[ \t]*(?:private|protected|public){1}(?:[ \t]static[ \t]){1}\b[a-zA-Z][a-zA-Z0-9<>]*\b[ \t]+([A-Za-z_][A-Za-z\d_]*)/i) 
+            $matchFunctionName = "";
+            if (  #get
+                (($matchFunctionName) = $codeLine =~ m/^[ \t]*(?:private|protected|public){1}[ \t]+\b([a-zA-Z][a-zA-Z0-9\][<>]*)\b[ \t]+(?:get){1}/i)
             ||
-                 (($matchFunctionName) = $codeLine =~ m/^[ \t]*(?:private|protected|public){1}[ \t]+\b[a-zA-Z][a-zA-Z0-9<>]*\b[ \t]+([A-Za-z_][A-Za-z\d_]*)/i)
+                #static 
+                (($matchFunctionName) = $codeLine =~ m/^[ \t]*(?:private|protected|public){1}(?:[ \t]static[ \t]){1}\b[a-zA-Z][a-zA-Z0-9\][<>]*\b[ \t]+([A-Za-z_][A-Za-z\d_]*)/i) 
+            ||
+                 (($matchFunctionName) = $codeLine =~ m/^[ \t]*(?:private|protected|public){1}[ \t]+\b[a-zA-Z][a-zA-Z0-9\][<>]*\b[ \t]+([A-Za-z_][A-Za-z\d_]*)/i)
+
             )
             {
                 ProcessFinalStuffForFunction();
-                $CurrentFunctionName = $matchFunctionName;
+                $CurrentFunctionName = $codeLine;
                 ChangeFunction();
                 $NextIsFunctionName = 0;
+            }
+            else
+            {
+              #not completed function
+              $NextIsFunctionName = 1;
             }
         }
         
         if ($NextIsFunctionName == 1)
         {
-            if ($codeLine =~ /^[ \t]*\b([A-Za-z_][A-Za-z\d_])\b[ \t]*\(/i)
-            {
-                $CurrentFunctionName = $1;
-                ChangeFunction();
-                $NextIsFunctionName = 0;
-            }
+#            if ($codeLine =~ /^[ \t]*\b([A-Za-z_][A-Za-z\d_])\b[ \t]*\(/i)
+              ProcessFinalStuffForFunction();
+              $CurrentFunctionName = $codeLine;
+              ChangeFunction();
+              $NextIsFunctionName = 0;
         }
 
     }
@@ -177,28 +250,42 @@ sub ChangeFunction()
     $StartFunctionLine = $lineIndex;
     print("New Function : $CurrentFunctionName\n");
 
+    foreach my $key (keys %vQueryVariables) {
+        delete $vQueryVariables{$key};
+    }
 
-    %{$vQueryVariables} = ();
+    foreach my $key (keys %vQuerySQL) {
+        delete $vQuerySQL{$key};
+    }
 
-
+    $CurrentSQL = "";
+    $CurrentQueryVariables = "";
     $IsInSQLQueryMode = 0;
     $StatisticsOfCurrentFunction{NVL} = 0;
     $StatisticsOfCurrentFunction{DateTime} = 0;
     $StatisticsOfCurrentFunction{BLOB} = 0;
     $StatisticsOfCurrentFunction{LOC} = 0;
     $StatisticsOfCurrentFunction{SQLQueryOccurence} = 0;
+    $StatisticsOfCurrentFunction{COUNT} = 0;
 }
 
 
 sub ProcessFinalStuffForFunction()
 {
     print("Process Final Stuff for Current Function : $CurrentFunctionName\n");
-    print("StartFunctionLine : $StartFunctionLine");
+    print("StartFunctionLine : $StartFunctionLine\n");
 
     foreach my $oneVariable (keys %vQueryVariables)
     {
        print(" -------------VQUERYVARIABLE COUNT : $vQueryVariables{$oneVariable}\n");
     }
+
+    foreach my $oneSQL (keys %vQuerySQL)
+    {
+       print(" ********************************SQL : $vQuerySQL{$oneSQL}\n");
+    }
+
+
 }
 
 
